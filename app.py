9bb9a1a71a170b60b7280c9858676f9fbc3ec7c8
namtannt <89174741+namtannt@users.noreply.github.com>
2021-11-22 13:58:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,10 +151,13 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if msg == "สวัสดี" : 
             replyObj = TextSendMessage(text="เออ...ดีด้วย")
+            line_bot_api.reply_message(rtoken, replyObj)    
         elif msg == "กินข้าวไหม" :
             replyObj = TextSendMessage(text="กินๆ")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "ไปเที่ยวกันไหม" :
-            replyObj = TextSendMessage(text="ไป") 
+            replyObj = TextSendMessage(text="ไป")
+            line_bot_api.reply_message(rtoken, replyObj)
         else :
             replyObj = TextSendMessage(text=msg) 
         line_bot_api.reply_message(rtoken, replyObj)
